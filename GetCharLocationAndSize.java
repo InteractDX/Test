548875720371ef55e8471535afeb7a7c91c3c709
waygazer <waygazer@acm.org>
2023-04-02 09:00:30 +0530

    new procedure findSubwords

--- GetCharLocationAndSize.java
+++ GetCharLocationAndSize.java
@@ -51,4 +51,54 @@ public class GetCharLocationAndSize extends PDFTextStripper {
                     text.getWidthDirAdj() + "]");
         }
     }
+
+    List<TextPositionSequence> findSubwords(PDDocument document, int page, String searchTerm) throws IOException
+    {
+        final List<TextPosition> allTextPositions = new ArrayList<>();
+        PDFTextStripper stripper = new PDFTextStripper()
+        {
+            @Override
+            protected void writeString(String text, List<TextPosition> textPositions) throws IOException
+            {
+                allTextPositions.addAll(textPositions);
+                super.writeString(text, textPositions);
+            }
+    
+            @Override
+            protected void writeLineSeparator() throws IOException {
+                if (!allTextPositions.isEmpty()) {
+                    TextPosition last = allTextPositions.get(allTextPositions.size() - 1);
+                    if (!" ".equals(last.getUnicode())) {
+                        Matrix textMatrix = last.getTextMatrix().clone();
+                        textMatrix.setValue(2, 0, last.getEndX());
+                        textMatrix.setValue(2, 1, last.getEndY());
+                        TextPosition separatorSpace = new TextPosition(last.getRotation(), last.getPageWidth(), last.getPageHeight(),
+                                textMatrix, last.getEndX(), last.getEndY(), last.getHeight(), 0, last.getWidthOfSpace(), " ",
+                                new int[] {' '}, last.getFont(), last.getFontSize(), (int) last.getFontSizeInPt());
+                        allTextPositions.add(separatorSpace);
+                    }
+                }
+                super.writeLineSeparator();
+            }
+        };
+        
+        stripper.setSortByPosition(true);
+        stripper.setStartPage(page);
+        stripper.setEndPage(page);
+        stripper.getText(document);
+    
+        final List<TextPositionSequence> hits = new ArrayList<TextPositionSequence>();
+        TextPositionSequence word = new TextPositionSequence(allTextPositions);
+        String string = word.toString();
+    
+        int fromIndex = 0;
+        int index;
+        while ((index = string.indexOf(searchTerm, fromIndex)) > -1)
+        {
+            hits.add(word.subSequence(index, index + searchTerm.length()));
+            fromIndex = index + 1;
+        }
+    
+        return hits;
+    }
 }
